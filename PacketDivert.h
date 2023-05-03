317295c58af170148fc53961dfbffa06f69b696d
forDecember <1921567337@qq.com>
2023-05-03 11:37:27 +0800

    添加了获取pid的逻辑，commitData可以得到pid了

--- PacketDivert.h
+++ PacketDivert.h
@@ -2,11 +2,13 @@
 #define __PACKETDIVERT_H__
 
 #include <WinSock2.h>
-#include <Windows.h> // 前面必须是winsock2
+#include <windows.h> // 前面必须是winsock2
 #include <iostream>
 #include <functional>
 #include <mutex>
 #include <thread>
+#include <map>
+#include <list>
 
 #include <psapi.h>
 #include <shlwapi.h>
@@ -15,16 +17,15 @@
 
 #include "include/windivert.h"
 
-typedef struct FLOW
-{
-	WINDIVERT_ADDRESS addr;
-	struct FLOW* next;
-} FLOW, * PFLOW;
 
 class PacketDivert
 {
+
 private:
 	HANDLE handle = nullptr;
+
+	std::map<UINT,UINT32>* mapPortWithPID = nullptr; /* 端口和pid的映射 */
+	std::list<WINDIVERT_ADDRESS> sniffedAddrList; /* 嗅探到的包地址信息 */
 
 
 	const char* packetFilter = nullptr;
@@ -41,7 +42,6 @@ private:
 	PWINDIVERT_TCPHDR tcpHeader = nullptr;
 
 	std::mutex myThreadMutex;
-	PFLOW flows = nullptr; /* 记录所有当前捕获的流 */
 
 public:
 	PacketDivert() = default;
@@ -50,7 +50,7 @@ public:
 
 	void startDivert(std::function<void(PWINDIVERT_IPHDR&, PWINDIVERT_TCPHDR&, WINDIVERT_ADDRESS&)> dealFunc);
 
-	void startDivert(std::function<void(WINDIVERT_ADDRESS)> dealFunc);
+	void startDivert(std::function<void(WINDIVERT_ADDRESS)> dealFunc,std::map<UINT,UINT32>* mapPortPID = nullptr);
 	void handleFlow();
 
 };
--- ProxyServer.h
+++ ProxyServer.h
@@ -6,12 +6,14 @@
 #include <WinSock2.h>
 #include <thread>
 #include <mutex>
+#include <map>
 #pragma comment(lib, "WinDivert.lib")
 #pragma comment(lib, "Ws2_32.lib")
 
 class ProxyServer
 {
 private:
+	std::map<UINT,UINT32>* mapPortWithPID = nullptr;
 	struct sockaddr_in serverSocketAddr {};
 	SOCKET serverSocketFD;
 	bool isLoop = false;
@@ -21,12 +23,12 @@ public:
 	ProxyServer(UINT proxyPort);
 	~ProxyServer();
 
-	void startServer(int maxWaitList, UINT altPort);
+	void startServer(int maxWaitList, UINT altPort,	std::map<UINT,UINT32>* mapPortPID = nullptr);
 
 private:
-	int transDataInner(SOCKET getDataSocketFD, SOCKET sendDataSocketFD, BOOL inbound);
+	int transDataInner(SOCKET getDataSocketFD, SOCKET sendDataSocketFD, BOOL inbound, UINT oriClientPort);
 
-	int commitData(const char* const originData, size_t lenOfOriData, char** newData, size_t* lenOfNewData);
+	int commitData(const char* const originData, size_t lenOfOriData,UINT32 pid, char** newData, size_t* lenOfNewData);
 
 };
 #endif
